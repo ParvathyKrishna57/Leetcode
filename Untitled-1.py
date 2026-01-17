f76b712c2266be092e76e7c8f522eb3b8dc2b659
ParvathyKrishna57 <parvathykrishnakrishna91@gmail.com>
2026-01-17 20:22:13 +0530

    to lowercase

--- Untitled-1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def countDigits(self, num: int) -> int:
-        count = 0
-        for d in str(num):
-            if num % int(d) == 0:
-                count += 1
-        return count
